0436247d8ced199eecb0a74560f53eaeb8daca11
mohametalmeari <mohamet.almeari@gmail.com>
2024-03-24 06:24:29 +0300

    Update App.js with 3D canvas and UI components

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Canvas } from "@react-three/fiber/native";
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { BottomUI } from "./components/BottomUI";
+import { Sandwich } from "./components/Sandwich";
+import { Suspense } from "react";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Canvas camera={{ position: [-2, 2.5, 5], fov: 30 }}>
+        <color attach="background" args={["#512DA8"]} />
+        <Suspense>
+          <Sandwich />
+        </Suspense>
+      </Canvas>
+      <BottomUI />
     </View>
   );
 }
@@ -13,8 +22,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
